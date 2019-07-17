31f4ef5a4e825650af4bb0ecc44af998e5b5410d
iroshiva <jean.weil@protonmail.com>
2019-07-17 15:34:08 +0200

    ajout like #bonjour_monde méthod .filter

--- lib/projet_twitter.rb
+++ lib/projet_twitter.rb
@@ -8,12 +8,12 @@ Dotenv.load('.env')
 
 def login_twitter
 	client = Twitter::REST::Client.new do |config|
-	  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-	  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-  	end
-  	return client
+		config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+		config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+		config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+		config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	end
+	return client
 end
 
 # ligne qui permet de tweeter sur ton compte
@@ -24,10 +24,74 @@ end
 # first_tweet(login_twitter)
 
 
+def random_items
+	array_journ = ["@jcunniet", "@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain"]
+	array_5_journ = array_journ.sample(5)
+	return array_5_journ
+end
 
-	# ce qui rentre ce sont les identifiants du client
-	# vérifie que le client est bien connecté
-	# sort soit client == 0 ou client existe
+def tweet_each_journ(array_5_journ)
+	client = login_twitter
+	array_5_journ.each do |journ|
+		client.update("#{journ} Je kiffe ton dernier post ! #bonjour_monde!")
+	end
+end
+
+def perform_random_tweet
+	# entrée : tableau
+	# action :
+	array_5_journ = random_items
+	# - prendre 5 journalistes au hasard dans le tableau
+	tweet_each_journ(array_5_journ)
+	# - à chaque journalistes ==> envoi d'un tweet se terminant par #bonjour_monde
+
+	# sortie : les 5 tweet
+end
+
+# perform_random_tweet
 
 
 
+def tweet_bonj_mond
+	# entrée : les tweets
+	client = login_twitter
+	# client.search("#bonjour_monde")
+	keywords = [‘bonjour‘, ‘monde‘]
+	client.filter(:track => keywords.join(“,“), result_type: "recent").take(25) do |tweet|
+		if tweet.is_a?(Twitter::Tweet)
+			# puts “#{object.id} #{object.user.screen_name} – #{object.text}“
+			# puts “———————————————“
+			# end
+			client.fav tweet
+		end
+		# action : trouver les tweets qui affichent #bonjour_monde
+		# sortie : au moins 25 tweets qui ont #bonjour_monde
+
+
+
+		# def like_hello
+		#    @client.search("#{@hashtbonjour}", result_type: "recent").take(25).collect do |tweet|
+		#      @client.favorite(tweet)
+		#    end
+		#  end
+	end
+
+
+	tweet_bonj_mond
+
+	# def like_tweet_bon_mond()
+	# entrée : résultat de la méthode tweet_bonj_mond
+	# action : like chacun des tweet bonjour_monde
+	# sortie : tweets likés
+	# end
+
+
+	# def perform_follow_bonjour
+	# méthode qui like les derniers tweet avec le #bonjour_monde
+	# entrée : tous les tweets
+	# action :
+	# chercher les tweet qui ont #bonjour_monde
+	# Pour chacun de ces tweets ==> liker
+	# sortie ==> les tweets #bonjour_monde likés
+
+	# end
